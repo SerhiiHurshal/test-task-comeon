7e102eab36eb678637cf081e3f002a4308f97da8
Serhii Hurshal <seergiygurshall@gmail.com>
2021-08-29 09:49:46 +0300

    add comment

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,6 +1,9 @@
 import nc from 'next-connect';
 import type { NextApiResponse } from 'next';
 
+/**
+ * Welocme api page
+ */
 const handler = nc().get((_, res: NextApiResponse) => {
   res.status(200).json({ text: 'Welcome!' });
 });
